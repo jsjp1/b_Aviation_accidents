be5ff8a77943385b4ee753562b4065e162a4d2f4
JIEEEN <tjtpp009@korea.ac.kr>
2025-01-07 15:03:50 +0900

    mod index body

--- init_crawl/index_body.py
+++ init_crawl/index_body.py
@@ -1,44 +1,48 @@
 index_body = {
-        "settings": {
-            "analysis": {
-                "analyzer": {
-                    "ngram_analyzer": {
-                        "tokenizer": "ngram_tokenizer",
-                        "filter": ["lowercase"]
-                    }
-                },
-                "tokenizer": {
-                    "ngram_tokenizer": {
-                        "type": "ngram",
-                        "min_gram": 3,
-                        "max_gram": 5
-                    }
+    "settings": {
+        "analysis": {
+            "analyzer": {
+                "ngram_analyzer": {
+                    "tokenizer": "ngram_tokenizer",
+                    "filter": ["lowercase"]
                 }
             },
-            "index": {
-                "max_ngram_diff": 2
+            "tokenizer": {
+                "ngram_tokenizer": {
+                    "type": "ngram",
+                    "min_gram": 2,
+                    "max_gram": 4 
+                }
             }
         },
-        "mappings": {
-            "properties": {
-                "date": {"type": "date"},
-                "time": {"type": "keyword"},
-                "airline": {
-                    "type": "text",
-                    "analyzer": "ngram_analyzer",
-                    "fields": {
-                        "raw": {"type": "keyword"}
-                    }
-                },
-                "aircraft_registration": {"type": "keyword"},
-                "accident_type": {"type": "keyword"},
-                "description": {"type": "text", "analyzer": "ngram_analyzer"},
-                "fatalities": {"type": "integer"},
-                "occupants": {"type": "integer"},
-                "location": {"type": "text", "analyzer": "ngram_analyzer"},
-                "phase": {"type": "keyword"},
-                "aircraft_model": {"type": "keyword"},
-                "aircraft_status": {"type": "keyword"}
-            }
+        "index": {
+            "max_ngram_diff": 3
+        }
+    },
+    "mappings": {
+        "properties": {
+            "date": {"type": "date"},
+            "time": {"type": "keyword"},
+            "airline": {
+                "type": "text",
+                "analyzer": "ngram_analyzer",
+                "fields": {
+                    "raw": {"type": "keyword"}
+                }
+            },
+            "aircraft_registration": {"type": "keyword"},
+            "accident_type": {"type": "keyword"},
+            "description": {"type": "text"},
+            "ko-description": {"type": "text"},
+            "fatalities": {"type": "integer"},
+            "occupants": {"type": "integer"},
+            "location": {
+                "type": "text",
+                "analyzer": "ngram_analyzer"
+            },
+            "phase": {"type": "keyword"},
+            "aircraft_model": {"type": "keyword"},
+            "aircraft_status": {"type": "keyword"}
         }
     }
+}
